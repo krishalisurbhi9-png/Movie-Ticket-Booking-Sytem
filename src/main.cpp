61d973ee462b9c7611149d57b3199e73b1c5ae33
krishalisurbhi9-png <krishalisurbhi9@gmail.com>
2026-09-07 23:04:26 +0530

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    cout << "======================================" << endl;
+    cout << "      CINESTAR MOVIE TICKET SYSTEM    " << endl;
+    cout << "======================================" << endl;
+
+    cout << "1. View Movies" << endl;
+    cout << "2. Book Ticket" << endl;
+    cout << "3. Cancel Ticket" << endl;
+    cout << "4. View Ticket" << endl;
+    cout << "5. Exit" << endl;
+
+    return 0;
+}
